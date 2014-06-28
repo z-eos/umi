7f2e3962bf444175a18c9584d884dde2211bad8c
z-eos <zeus.panchenko@gmail.com>
2014-06-28 18:46:32 +0300

    cat controllers for choose and add

---
+++ lib/UMI/Controller/LDAP_organization_add.pm
@@ -0,0 +1,179 @@
+package UMI::Controller::LDAP_organization_add;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; with 'Tools'; }
+
+use UMI::Form::LDAP_organization_add;
+
+=head1 NAME
+
+UMI::Controller::LDAP_organization_add - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+has 'form' => ( isa => 'UMI::Form::LDAP_organization_add', is => 'rw',
+		lazy => 1, 
+		default => sub { 
+		  UMI::Form::LDAP_organization_add->new('form_element_class' => 'form-horizontal')
+		  },
+		documentation => q{Form to add organization/s},
+	      );
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c, $ldap_org_id ) = @_;
+    if ( $c->check_user_roles('umi-admin')) {
+      # use Data::Dumper;
+
+      $c->stash( template => 'ldapact/ldapact_o_add_wrap.tt',
+		 form => $self->form );
+
+      # use Data::Dumper;
+      # $c->log->debug( "\$form:\n" . Dumper($self->form));
+
+      # Validate and insert/update database
+      return unless $self->form->process( item_id => $ldap_org_id,
+					  posted => ($c->req->method eq 'POST'),
+					  params => $c->req->parameters,
+					  ldap_crud => $c->model('LDAP_CRUD'),
+					  uid => $c->session->{umi_ldap_uid},
+					  pwd => $c->session->{umi_ldap_password}
+					);
+
+      my $res = $self->create_object( $c );
+      $c->log->debug( "create_object (from umi_o_add) error: " . $res) if $res;
+
+    } else {
+      $c->response->body('Unauthorized!');
+    }
+}
+
+=head2 create_object
+
+=cut
+
+sub create_object {
+  my  ( $self, $c ) = @_;
+  my $attrs = $c->req->parameters;
+
+  use Data::Dumper;
+  # $c->log->debug( "\$attrs:\n" . Dumper($attrs));
+
+  my $ldap = 
+    $c->model('LDAP_CRUD')->umi_bind({
+				      dn => 'uid=' .
+				      $c->session->{umi_ldap_uid} .
+				      ',ou=people,dc=ibs',
+				      password => $c->session->{umi_ldap_password}
+				     });
+
+  my $descr = 'description has to be here';
+  if (defined $attrs->{'descr'} and $attrs->{'descr'} ne '') {
+    $descr = join(' ', $attrs->{'descr'});
+  }
+
+  my $telephoneNumber = '666';
+  if (defined $attrs->{'telephonenumber'} and $attrs->{'telephonenumber'} ne '') {
+    $telephoneNumber = $attrs->{'telephonenumber'};
+  }
+
+#
+## HERE WE NEED TO SET FLAG TO CREATE BRANCH FOR LOCALIZED VERSION OF DATA
+## associatedService=localization-ru,uid=U...-user01,ou=People,dc=ibs
+## associatedService=localization-uk,uid=U...-user01,ou=People,dc=ibs
+## e.t.c.
+#
+
+  my ( $givenName, $sn, $cn, $o, $pwd );
+  if ($self->is_ascii($attrs->{'fname'})) {
+    $givenName = $self->cyr2lat({ to_translate => $attrs->{'fname'} });
+  } else {
+    $givenName = $attrs->{'fname'};
+  };
+  $cn = $givenName;
+
+  if ($self->is_ascii($attrs->{'lname'})) {
+    $sn = $self->cyr2lat({ to_translate => $attrs->{'lname'} });
+  } else {
+    $sn = $attrs->{'lname'};
+  };
+  $cn .= ' ' . $sn;
+
+  if ($self->is_ascii($attrs->{'org'})) {
+    $o = $self->cyr2lat({ to_translate => $attrs->{'org'} });
+  } else {
+    $o = $attrs->{'org'};
+  };
+
+  my $attr_defined = [];
+  foreach my $key (keys %{$attrs}) {
+    push @{$attr_defined}, $key => $self->cyr2lat({ to_translate => $attrs->{$key}})
+      if defined $attrs->{$key} and $attrs->{$key} ne '' and $key ne 'submit' and $key ne 'parent';
+  }
+  push @{$attr_defined}, objectClass => [ 'top', 'organizationalUnit' ];
+
+  $c->log->debug( "parent\n: $attrs->{'parent'} \nattr_defined:\n" . Dumper($attr_defined));
+  my $success_message;
+
+  ######################################################################
+  # ORGANIZATION Object
+  ######################################################################
+  my $base = defined $attrs->{'parent'} ? $attrs->{'parent'} : 'ou=Organizations,dc=ibs';
+  my $ldif =
+    $c->model('LDAP_CRUD')->umi_add($ldap,
+				    'ou=' . $attrs->{'ou'} . ',' .
+				    $base,
+				    $attr_defined,
+				    0);
+  my $error_message;
+  if ( $ldif ) {
+    $error_message = 'Error during organization object creation occured: ' . $ldif;
+    $c->log->error('ou=' . $attrs->{'ou'} .
+		   ',ou=Organizations,dc=ibs' .
+		   " obj wasn not created: " . $ldif);
+  }
+
+  my $final_message;
+  $final_message = '<div class="alert alert-success">' .
+    '<span style="font-size: 140%" class="glyphicon glyphicon-ok-sign"></span>' .
+      '&nbsp;<em>Object for organization ' .
+	' &laquo;' . $self->cyr2lat({ to_translate => $attrs->{'physicaldeliveryofficename'} }) .
+	  '&raquo;</em> successfully created.</div>' if $success_message;
+
+  $final_message .= '<div class="alert alert-danger">' .
+    '<span style="font-size: 140%" class="icon_error-oct" aria-hidden="true"></span>&nbsp;' .
+      $error_message . '</div>' if $error_message;
+
+  $self->form->info_message( $final_message ) if $final_message;
+
+  $ldap->unbind;
+  return $ldif;
+}
+
+
+=head1 AUTHOR
+
+Charlie &
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ lib/UMI/Controller/LDAP_organization_select.pm
@@ -0,0 +1,150 @@
+package UMI::Controller::LDAP_organization_select;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; with 'Tools'; }
+
+use UMI::Form::LDAP_organization_select;
+use UMI::LDAP_CRUD;
+
+has 'form' => ( isa => 'UMI::Form::LDAP_organization_select', is => 'rw',
+		lazy => 1, default => sub { UMI::Form::LDAP_organization_select->new },
+		documentation => q{Form to add/mod/del organization/s},
+	      );
+
+=head1 NAME
+
+UMI::Controller::LDAP_organization_select - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c, $ldap_org_id ) = @_;
+    if ( $c->check_user_roles('umi-admin')) {
+      # use Data::Dumper;
+
+      $c->stash( template => 'ldapact/ldapact_o_wrap.tt',
+		 form => $self->form );
+
+      # use Data::Dumper;
+      # $c->log->debug( "uid and pwd:\n" . Dumper($self->form));
+
+      # $c->log->debug( "uid and pwd:\n" . $c->session->{umi_ldap_uid} . "\n" . $c->session->{umi_ldap_password});
+
+      # Validate and insert/update database
+      return unless $self->form->process( item_id => $ldap_org_id,
+					  posted => ($c->req->method eq 'POST'),
+					  params => $c->req->parameters,
+					  ldap_crud => $c->model('LDAP_CRUD'),
+					  uid => $c->session->{umi_ldap_uid},
+					  pwd => $c->session->{umi_ldap_password}
+					);
+
+      my $ldap = 
+	$c->model('LDAP_CRUD')->umi_bind({
+					  dn => 'uid=' .
+					  $c->session->{umi_ldap_uid} .
+					  ',ou=people,dc=ibs',
+					  password => $c->session->{umi_ldap_password}
+					 });
+
+      my ( $error_message, $success_message, $message);
+
+      if ( not $c->req->param('act') ) {
+	$c->log->debug( "we will add new Organization object");
+	$c->res->redirect($c->uri_for('/ldap_organization_add'));
+	# $c->controller('LDAP_organization_add')->index($c, ( physicaldeliveryofficename => $c->req->param('neworg') ));
+      } elsif ( $c->req->param('act') == 1 ) {
+	$c->log->debug( "we will modify object " . $c->req->param('org'));
+	$c->controller('LDAP_organization_mod')->index($c, ( org => $c->req->param('org') ));
+      } elsif ( $c->req->param('act') == 2 ) {
+	my $delete_result = $c->model('LDAP_CRUD')->umi_del($ldap, $c->req->param('org'), 0);
+	if ($delete_result) {
+	  $error_message = '<li>&laquo;<strong>' . $c->req->param('org') .
+	    '</strong>&raquo; <em>was not deleted; error is:</em> &laquo;<strong>' .
+	      $delete_result  . '</strong>&raquo;</li>';
+	  $c->log->debug( $delete_result );
+	} else {
+	  $success_message = '<li>&laquo;<strong>' . $c->req->param('org') .
+	    '</strong>&raquo; <em>was successfully deleted.</em></li>';
+	  $self->form->success_message( $success_message );
+	  $c->res->redirect($c->uri_for('/ldap_organization_select'));
+	}
+      }
+
+      $ldap->unbind;
+
+      my $final_message;
+      $final_message = '<div class="alert alert-success">' .
+	'<span style="font-size: 140%" class="glyphicon glyphicon-ok-sign"></span>' .
+	  '&nbsp;<em>Operation complete.</em><ul>' . $success_message . '<ul></div>' if $success_message;
+
+      $final_message .= '<div class="alert alert-danger">' .
+	'<span style="font-size: 140%" class="icon_error-oct" aria-hidden="true"></span><ul>' .
+	  $error_message . '</ul></div>' if $error_message;
+
+      $self->form->info_message( $final_message ) if $final_message;
+
+    } else {
+      $c->response->body('Unauthorized!');
+    }
+}
+
+=head2 create_account
+
+=cut
+
+
+sub add :Path(add) :Args(0) {
+    my ( $self, $c, $ldap_org_id ) = @_;
+    if ( $c->check_user_roles('umi-admin')) {
+      # use Data::Dumper;
+
+      $c->stash( template => 'ldapact/ldapact_o_add_wrap.tt',
+		 form => $self->form );
+
+      # use Data::Dumper;
+      # $c->log->debug( "\$form:\n" . Dumper($self->form));
+
+      # Validate and insert/update database
+      return unless $self->form->process( item_id => $ldap_org_id,
+					  posted => ($c->req->method eq 'POST'),
+					  params => $c->req->parameters,
+					  ldap_crud => $c->model('LDAP_CRUD'),
+					  uid => $c->session->{umi_ldap_uid},
+					  pwd => $c->session->{umi_ldap_password}
+					);
+
+#    $c->response->body('Matched UMI::Controller::LDAP_organization_add in LDAP_organization_add.');
+    } else {
+      $c->response->body('Unauthorized!');
+    }
+}
+
+
+
+=head1 AUTHOR
+
+Zeus
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
