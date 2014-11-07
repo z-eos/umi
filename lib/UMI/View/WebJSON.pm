d6bbb59fbfc8d3f9a3ac7cd43867063854290682
Zeus Panchenko <zeus@foo.bar>
2014-11-07 11:09:43 +0200

    lib/UMI/View/WebJSON.pm			added for js delete functional root/src/user/user_preferences.tt	dropdown meny for services in user_preferences performed with <ul>

---
+++ lib/UMI/View/WebJSON.pm
@@ -0,0 +1,35 @@
+package UMI::View::WebJSON;
+
+use strict;
+use base 'Catalyst::View::JSON';
+
+__PACKAGE__->config({
+		     allow_callback  => 1,
+		     callback_param  => 'cb',
+		     expose_stash    => [ qw(success message) ],
+		    });
+
+=head1 NAME
+
+UMI::View::WebJSON - Catalyst JSON View
+
+=head1 SYNOPSIS
+
+See L<UMI>
+
+=head1 DESCRIPTION
+
+Catalyst JSON View.
+
+=head1 AUTHOR
+
+Zeus Panchenko
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
