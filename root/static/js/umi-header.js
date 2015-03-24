f8440b3c3158d23197629b811dce71b1a044a596
Zeus Panchenko <zeus.panchenko+it@gmail.com>
2015-03-24 20:58:07 +0200

    header fixes

--- root/static/js/umi-header.js
+++ root/static/js/umi-header.js
@@ -1,16 +1,2 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-});
 
-
-// navbar search field submit by key "enter" press
-$( '.umi-navbar-menu-btn' ).click( function () {
-    $('.navbar-form').submit();
-});
-
-$('[name=ldapsearch_filter]').on('keypress', function (event) {
-    if ( event.which === 13 ) {
-        $('[name=ldapsearch_by_name]').trigger('click');
-    }
-});
-
+// here js have to reside
