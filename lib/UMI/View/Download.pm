9e0752e6f82ad7d8f9165d381920510b404fd2f4
Zeus Panchenko <zeus.panchenko+it@gmail.com>
2015-11-04 15:42:58 +0200

    vCard export added

---
+++ lib/UMI/View/Download.pm
@@ -0,0 +1,29 @@
+package UMI::View::Download;
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::View::Download';
+
+=head1 NAME
+
+UMI::View::Download - Download View for UMI
+
+=head1 DESCRIPTION
+
+Download View for UMI.
+
+=head1 SEE ALSO
+
+L<UMI>
+
+=head1 AUTHOR
+
+Zeus Panchenko
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+1;
