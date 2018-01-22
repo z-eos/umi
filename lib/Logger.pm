6928055ecb6ae601069eb5df6a0aa73a308d01d8
Zeus Panchenko <zeus.panchenko+it@gmail.com>
2018-01-22 16:43:01 +0200

    Logger developing start

--- lib/Logger.pm
+++ lib/Logger.pm
@@ -8,34 +8,43 @@ use Data::Printer;
 use Try::Tiny;
 use POSIX qw(strftime);
 
-use base 'Log::Contextual';
-# use Log::Contextual;
-use Log::Log4perl qw(:levels :easy);
-
-my $appender_file =
-  Log::Log4perl::Appender->new(
-			       "Log::Log4perl::Appender::File",
-			       name       => "umi_log",
-			       filename   => '/tmp/umi/umi.log',
-			       mode       => 'append',
-			       additivity => 0,
-			       utf8       => 1,
-			      );
-
-Log::Log4perl::init( \$appender_file );
+# use base 'Log::Contextual';
+use Log::Log4perl qw(:easy);
+Log::Log4perl->easy_init($DEBUG);
+my $logger  = Log::Log4perl->get_logger;
+set_logger $logger;
 
 
-sub arg_default_logger { $_[1] || Log::Log4perl->get_logger }
 
-sub arg_levels { [qw(debug trace warn info error fatal custom_level)] }
 
-sub default_import { ':log' }
+# use base 'Log::Contextual';
+# # use Log::Contextual;
+# use Log::Log4perl qw(:levels :easy);
 
-# or maybe instead of default_logger
-sub arg_package_logger { $_[1] }
+# my $appender_file =
+#   Log::Log4perl::Appender->new(
+# 			       "Log::Log4perl::Appender::File",
+# 			       name       => "umi_log",
+# 			       filename   => '/tmp/umi/umi.log',
+# 			       mode       => 'append',
+# 			       additivity => 0,
+# 			       utf8       => 1,
+# 			      );
 
-# and almost definitely not this, which is only here for completeness
-sub arg_logger { $_[1] }
+# Log::Log4perl::init( \$appender_file );
+
+
+# sub arg_default_logger { $_[1] || Log::Log4perl->get_logger }
+
+# sub arg_levels { [qw(debug trace warn info error fatal custom_level)] }
+
+# sub default_import { ':log' }
+
+# # or maybe instead of default_logger
+# sub arg_package_logger { $_[1] }
+
+# # and almost definitely not this, which is only here for completeness
+# sub arg_logger { $_[1] }
 
 
 =head1 AUTHOR
