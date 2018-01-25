a3bfd4fe3413f830101c2f05392f1f308d4e7a98
Zeus Panchenko <zeus.panchenko+it@gmail.com>
2018-01-25 07:44:33 +0200

    Log4perl java config style: work

--- lib/Logger.pm
+++ lib/Logger.pm
@@ -3,92 +3,55 @@
 
 package Logger;
 
-use Data::Printer;
-
 use Log::Log4perl qw(:levels :easy);
-use Log::Log4perl::Layout;
-use Log::Log4perl::Level;
-use Log::Log4perl::Filter;
-use Log::Log4perl::Filter::LevelRange;
-use Log::Log4perl::Filter::LevelMatch;
 
 use base qw(Log::Dispatch::Output);
 use base 'Log::Contextual';
 
+### OFF FATAL ERROR WARN INFO DEBUG TRACE ALL
 ### DEBUG, INFO, WARN, ERROR, FATAL
 
-###=== APPENDER FILE ==================================================
-my $apn_file =
-  Log::Log4perl::Appender->new(
-			       "Log::Log4perl::Appender::File",
-			       name       => 'appndr_f',
-			       filename   => '/tmp/umi/umi-transcript.log',
-			       mode       => 'append',
-			       additivity => 0,
-			       utf8       => 1,
-			       #	syswrite   => 1,
-			       recreate   => 1,
-			       mkpath     => 1
-			      );
+Log::Log4perl::init(\ <<'EOT');
 
-my $flt_file =
-  Log::Log4perl::Filter::LevelRange->new(
-					 LevelMin      => 'INFO',
-					 LevelMax      => 'FATAL',
-					 AcceptOnMatch => true
-					);
-$apn_file->filter($flt_file);
+  log4perl.logger                        = TRACE, apn_sysl, apn_file, apn_scrn
 
-my $layout_file =
-  Log::Log4perl::Layout::PatternLayout->new( "%d{yyyy.MM.DD HH:mm:ss} [%p]: L%05L @ %F{2}: %m{chomp}%n%n" );
+  log4perl.filter.flt_sysl               = Log::Log4perl::Filter::LevelRange
+  log4perl.filter.flt_sysl.LevelMin      = INFO
+  log4perl.filter.flt_sysl.LevelMax      = FATAL
+  log4perl.filter.flt_sysl.AcceptOnMatch = true
+  
+  log4perl.filter.flt_file               = Log::Log4perl::Filter::LevelMatch
+  log4perl.filter.flt_file.LevelToMatch  = DEBUG
+  log4perl.filter.flt_file.AcceptOnMatch = true
 
-$apn_file->layout($layout_file);
+  log4perl.appender.apn_scrn             = Log::Log4perl::Appender::ScreenColoredLevels  
+  log4perl.appender.apn_scrn.layout      = PatternLayout
+  log4perl.appender.apn_scrn.layout.ConversionPattern = %d{yyyy.MM.DD HH:mm:ss} [%p]: L%05L @ %F{2}: %m{chomp}%n%n
+  log4perl.appender.apn_scrn.Filter      = flt_sysl
 
+  log4perl.appender.apn_file             = Log::Log4perl::Appender::File
+  log4perl.appender.apn_file.layout      = PatternLayout
+  log4perl.appender.apn_file.layout.ConversionPattern = %d{yyyy.MM.DD HH:mm:ss} [%p]: L%05L @ %F{2}: %m{chomp}%n%n
+  log4perl.appender.apn_file.recreate    = 1
+  log4perl.appender.apn_file.mkpath      = 1
+  log4perl.appender.apn_file.filename    = /tmp/umi/umi-transcript.log
+  log4perl.appender.apn_file.mode        = append
+  log4perl.appender.apn_file.utf8        = 1
+  log4perl.appender.apn_file.additivity  = 0
+  log4perl.appender.apn_file.Filter      = flt_file
 
-### === APPENDER SYSLOG ================================================
-my $apn_sysl =
-  Log::Log4perl::Appender->new(
-			       "Log::Dispatch::Syslog",
-			       ident      => 'UMI',
-			       facility   => 'local2'
-			      );
+  log4perl.appender.apn_sysl             = Log::Dispatch::Syslog
+  log4perl.appender.apn_sysl.ident       = UMI
+  log4perl.appender.apn_sysl.facility    = local2
+  log4perl.appender.apn_sysl.layout      = PatternLayout
+  log4perl.appender.apn_sysl.layout.ConversionPattern = [%p]: L%05L @ %F{2}: %m{chomp}
+  log4perl.appender.apn_sysl.Filter      = flt_sysl
 
-my $flt_sysl =
-  Log::Log4perl::Filter::LevelMatch->new(
-					 LevelToMatch  => 'DEBUG',
-					 AcceptOnMatch => true
-					);
-$apn_sysl->filter($flt_sysl);
+EOT
 
-my $layout_sysl =
-  Log::Log4perl::Layout::PatternLayout->new( "[%p]: L%05L @ %F{2}: %m{chomp}" );
-
-$apn_sysl->layout($layout_sysl);
-
-### === APPENDER SCREEN ================================================
-my $apn_scrn =
-  Log::Log4perl::Appender->new(
-			       "Log::Log4perl::Appender::ScreenColoredLevels"
-			      );
-
-$apn_scrn->filter($flt_sysl);
-
-my $layout_scrn =
-  Log::Log4perl::Layout::PatternLayout->new( "%d{yyyy.MM.DD HH:mm:ss} [%p]: L%05L @ %F{2}: %m{chomp}%n%n" );
-
-$apn_scrn->layout($layout_scrn);
-
-
-
-
-get_logger->add_appender($apn_file);
-get_logger->add_appender($apn_sysl);
-get_logger->add_appender($apn_scrn);
-
-get_logger->level($TRACE);
 
 sub arg_default_logger { $_[1] || Log::Log4perl->get_logger }
-sub arg_levels { [qw(off fatal error warn info debug trace all)] }
+sub arg_levels { [qw(fatal error warn info debug)] }
 sub default_import { ':log' }
 
 
