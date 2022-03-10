4d9eec29e3a06aa1e01b5d7298d2dc36a8c607b9
Zeus Panchenko <zeus.panchenko+it@gmail.com>
2022-03-10 11:23:29 +0100

    fixes

--- lib/UMI.pm
+++ lib/UMI.pm
@@ -36,8 +36,9 @@ use Catalyst qw/
 
 extends 'Catalyst';
 
-our $VERSION = '0.91';
+our $VERSION = '0.92';
 
+__PACKAGE__->path_to($ENV{UMI_CONFIG_DIR});
 __PACKAGE__
   ->config({
 	    'Plugin::Cache' => { backend => { class => "Cache::Memory", }, },
